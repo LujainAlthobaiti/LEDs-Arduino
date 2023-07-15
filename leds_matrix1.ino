e48bf550e4e65d786c4042e47fb83758bc2e488f
Lujain Althobaiti <139415674+LujainAlthobaiti@users.noreply.github.com>
2023-07-16 01:59:39 +0300

    Update leds_matrix1.ino

--- leds_matrix1.ino
+++ leds_matrix1.ino
@@ -1,6 +1,8 @@
 // C++ code
-//
+
+//Define ports
 int leds = 7;
+
 void setup()
 {
   pinMode(7, OUTPUT);
@@ -8,8 +10,13 @@ void setup()
 
 void loop()
 {
+  //Turn on LEDs
   digitalWrite(leds, HIGH);
+  
   delay(1000); // Wait for 1000 millisecond(s)
+  
+  //Turn off LEDs
   digitalWrite(leds, LOW);
+  
   delay(1000); // Wait for 1000 millisecond(s)
 }
